db10509516b287090102d2a3be5a8e042771f942
Redmi <Redmi@LAPTOP-AOV61HTP>
2024-11-27 12:00:29 +0300

    Add project files.

---
+++ Arheo.Application/Repositories/IFileMetadata.cs
@@ -0,0 +1,12 @@
+﻿using Arheo.Domain;
+
+namespace Arheo.Application.Repositories
+{
+    public interface IFileMetadata
+    {
+        Task<List<FileMetadata>> GetAllFilesAsync();
+        Task<FileMetadata> GetFileByIdAsync( Guid id );
+        Task AddFileAsync( FileMetadata file );
+        Task DeleteFileAsync( Guid id );
+    }
+}
---
+++ Arheo.Domain/FileMetadata.cs
@@ -0,0 +1,10 @@
+﻿namespace Arheo.Domain
+{
+    public class FileMetadata
+    {
+        public Guid Id { get; set; }
+        public string FileName { get; set; }
+        public string Author { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
